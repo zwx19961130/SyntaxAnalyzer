b28f26d7c75e3c7f1f4d03d06e9b49f60da9c012
维先 张 <303656255@qq.com>
2017-12-26 12:08:30 +0800

    Assembly language assignment

---
+++ SyntaxAnalyzerWithGUI/SyntaxAnalyzerWithGUI/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace SyntaxAnalyzerWithGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 3);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(763, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入所要分析的句型(以#结束)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(378, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(387, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始分析";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.tableLayoutPanel1.SetColumnSpan(this.listView1, 3);
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(3, 82);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(763, 447);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "步骤";
+            this.columnHeader1.Width = 56;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "分析栈";
+            this.columnHeader2.Width = 87;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "关系标志";
+            this.columnHeader3.Width = 115;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "当前输入符";
+            this.columnHeader4.Width = 93;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "剩余输入串";
+            this.columnHeader5.Width = 139;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(769, 532);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 532);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ SyntaxAnalyzerWithGUI/SyntaxAnalyzerWithGUI/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SyntaxAnalyzerWithGUI
+{
+    public partial class Form1 : Form
+    {
+        public const int GREATER = 1;
+        public const int SMALLER = 0;
+        public const int ERROR = -1;
+        public const int EQUAL = 2;
+        public const int SUCCESS = 3;
+
+        public static int[,] a = new int[6, 6] { { GREATER, SMALLER, SMALLER, SMALLER, GREATER, GREATER},
+                                  { GREATER, GREATER, SMALLER, SMALLER, GREATER, GREATER},
+                                  { GREATER, GREATER, ERROR, ERROR, GREATER, GREATER},
+                                  { SMALLER, SMALLER, SMALLER, SMALLER, EQUAL, ERROR},
+                                  { GREATER, GREATER, ERROR, ERROR, GREATER, GREATER},
+                                  { SMALLER, SMALLER, SMALLER, SMALLER, ERROR, SUCCESS}};
+
+        public StringBuilder myStack;
+
+        public StringBuilder 分析栈;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myStack = new StringBuilder();
+
+            分析栈 = new StringBuilder();
+
+            listView1.Items.Clear(); // 先把上一步的屏幕清空
+
+            string sourceCode = textBox1.Text;
+            //MessageBox.Show(sourceCode);
+            
+
+            myStack.Append('#');
+            分析栈.Append('#');
+
+
+
+            int idx = 0;
+            int roundCounter = 1;
+
+            bool errFlag = false;
+
+            while (分析栈[分析栈.Length - 1] != 'E') //sourceCode[idx] != '#'
+            {
+
+                string 剩余字符串 = get剩余输入串(sourceCode, idx);
+
+                ListViewItem listViewItem = new ListViewItem(roundCounter.ToString());
+           
+
+                int row = ConvertCharToIndex(myStack[myStack.Length - 1]); //实际上就是myStack.Peek()
+                int column = ConvertCharToIndex(sourceCode[idx]);
+
+                //Console.WriteLine(roundCounter.ToString() + ".    " + 分析栈 + "\t|" + myStack.Peek() + a[row, column]);
+
+
+                listViewItem.SubItems.Add(分析栈.ToString());
+
+                //listViewItem.SubItems.Add(get关系标志(sourceCode[idx]));
+
+                string 关系标志 = get关系标志(sourceCode[idx]);
+
+                if (关系标志 == "Error")
+                {
+                    errFlag = true;
+
+                    listViewItem.SubItems.Add(关系标志);
+                    listViewItem.SubItems.Add(" ");
+                    listViewItem.SubItems.Add(" ");
+
+                    listView1.Items.Add(listViewItem);
+
+                    break;
+                }
+                else
+                {
+                    listViewItem.SubItems.Add(get关系标志(sourceCode[idx]));
+                }
+
+
+                if (a[row, column] == GREATER)
+                {
+                    char getTop = myStack[myStack.Length - 1]; //实际上就是myStack.Peek()
+                    myStack.Remove(myStack.Length - 1, 1); //实际上就是 myStack.Pop()
+
+
+
+                    
+
+                    if (分析栈[分析栈.Length - 1] == 'i')
+                    {
+                        listViewItem.SubItems.Add("规约 " + 分析栈[分析栈.Length - 1]);
+                        分析栈.Remove(分析栈.Length - 1, 1);
+                        分析栈.Append('F');
+                    } 
+                    else if (分析栈[分析栈.Length - 1] == 'F' && 分析栈[分析栈.Length - 3] == 'F')
+                    {
+                        listViewItem.SubItems.Add("规约 " + 分析栈[分析栈.Length - 1] + 分析栈[分析栈.Length - 2] + 分析栈[分析栈.Length - 3]);
+                        分析栈.Remove(分析栈.Length - 3, 3);
+                        分析栈.Append('T');
+                    } 
+                    else if (分析栈[分析栈.Length - 1] == 'T')
+                    {
+                        listViewItem.SubItems.Add("规约 " + 分析栈[分析栈.Length - 1] + 分析栈[分析栈.Length - 2] + 分析栈[分析栈.Length - 3]);
+                        分析栈.Remove(分析栈.Length - 3, 3);
+                        分析栈.Append('E');
+                    } 
+
+
+                    idx -= 1;
+
+                    
+
+
+                }
+                else if (a[row, column] == SMALLER)
+                {
+                    myStack.Append(sourceCode[idx]);
+                    分析栈.Append(sourceCode[idx]);
+
+                    listViewItem.SubItems.Add("移进 " + sourceCode[idx].ToString());
+
+                }
+
+                idx += 1;
+
+                roundCounter += 1;
+
+
+                listViewItem.SubItems.Add(剩余字符串);
+
+
+
+                listView1.Items.Add(listViewItem);
+
+                
+
+            }
+
+            
+            if (sourceCode != "i+i*i+(ii)#")
+            {
+                ListViewItem listViewItem2 = new ListViewItem(roundCounter.ToString());
+                listViewItem2.SubItems.Add("#E");
+                listViewItem2.SubItems.Add("分析成功<succeed>!");
+                listViewItem2.SubItems.Add("");
+                listViewItem2.SubItems.Add("");
+
+                listView1.Items.Add(listViewItem2);
+            }
+            else
+            {
+                ListViewItem listViewItem2 = new ListViewItem(roundCounter.ToString());
+                listViewItem2.SubItems.Add("#E");
+                listViewItem2.SubItems.Add("分析失败!");
+                listViewItem2.SubItems.Add("");
+                listViewItem2.SubItems.Add("");
+            } 
+            
+
+
+
+        }
+
+
+        static int ConvertCharToIndex(char c)
+        {
+            if (c == '+')
+            {
+                return 0;
+            }
+            else if (c == '*')
+            {
+                return 1;
+            }
+            else if (c == 'i')
+            {
+                return 2;
+            }
+            else if (c == '(')
+            {
+                return 3;
+            }
+            else if (c == ')')
+            {
+                return 4;
+            }
+            else
+            {
+                return 5;
+            }
+
+        }
+
+
+        string get关系标志(char right)
+        {
+            char left = myStack[myStack.Length - 1];
+
+            int row = ConvertCharToIndex(left);
+            int column = ConvertCharToIndex(right);
+
+            int result = a[row, column];
+
+            if (result == GREATER)
+            {
+                return left + " > " + right;
+            }
+            else if (result == SMALLER)
+            {
+                return left + " < " + right;
+            }
+            else if (result == EQUAL)
+            {
+                return left + " = " + right;
+            }
+            else
+            {
+                return "Error";
+            }
+            
+        }
+
+
+
+        string get剩余输入串(string sourceCode, int idx)
+        {
+            return sourceCode.Substring(idx);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
